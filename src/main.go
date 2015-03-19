f854790e6b373efeb18a4aa2eb5be04dd7ed88b5
Adam Drake <adrake@gmail.com>
2015-03-19 20:28:06 +0800

    Initial commit

---
+++ src/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	redis "gopkg.in/redis.v2"
+	msgpack "gopkg.in/vmihailenco/msgpack.v2"
+	"log"
+	"net"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Metric struct {
+	name        string
+	measurement Measurement
+}
+
+type Measurement struct {
+	value     float64
+	timestamp float64
+}
+
+func handleMetric(inq chan []byte, outq chan Metric, client *redis.Client, loopcount *uint64, loopstart *time.Time, wg *sync.WaitGroup) {
+	//wg.Add(1)
+	defer wg.Done()
+	pipe := client.Pipeline()
+
+	var pipecount uint32 = 0
+	for item := range inq {
+		message := string(item)
+		messageCleaned := strings.TrimSuffix(message, "\n")
+		splitLine := strings.Split(messageCleaned, " ")
+		//mpval, _ := msgpack.Marshal(splitLine[1] + " " + splitLine[2])
+		val, _ := strconv.ParseFloat(splitLine[1], 64)
+		ts, _ := strconv.ParseFloat(splitLine[2], 64)
+
+		mpval, _ := msgpack.Marshal(Measurement{value: val, timestamp: ts})
+		pipe.Append(splitLine[0], string(mpval))
+		pipe.SAdd("metricNames", splitLine[0])
+		pipecount += 1
+
+		if pipecount > 512 {
+			cmds, err := pipe.Exec()
+			if (*loopcount)%10000 == 0 {
+				fmt.Println(cmds)
+			}
+			check(err)
+			pipecount = 0
+		}
+
+		if *loopcount%10000 == 0 {
+			fmt.Println("rate:", float64(*loopcount)/time.Since(*loopstart).Seconds())
+		}
+
+		*loopcount += 1
+
+	}
+
+}
+
+func startListening(inq chan []byte, outq chan Metric) {
+	addr, _ := net.ResolveUDPAddr("udp", ":2001")
+	sock, err := net.ListenUDP("udp", addr)
+	check(err)
+	for {
+		buf := make([]byte, 512)
+		size, _, err := sock.ReadFromUDP(buf)
+		check(err)
+		inq <- buf[0:size]
+	}
+}
+
+func trimMetrics(maxlen int, client *redis.Client, interval int) {
+	for {
+		metricNames, err := client.SMembers("metricNames").Result()
+		check(err)
+		for _, name := range metricNames {
+			var vals []Measurement
+			rawvals, err := client.Get(name).Result()
+			check(err)
+			err = msgpack.Unmarshal([]byte(rawvals), &vals)
+			check(err)
+			outvals, _ := msgpack.Marshal(vals[0:maxlen])
+			client.Set(name, string(outvals))
+		}
+		time.Sleep(time.Second * time.Duration(interval))
+	}
+
+}
+
+func main() {
+	startTime := time.Now()
+
+	WORKER_COUNT := runtime.NumCPU() * 2
+	MAX_METRICS := 5000
+	TRIM_INTERVAL := 1
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	logFile, err := os.OpenFile("info.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	defer logFile.Close()
+	check(err)
+	logger := log.New(logFile, "", log.LstdFlags)
+	logger.Println("starting execution at", startTime)
+
+	client := redis.NewClient(&redis.Options{Network: "unix", Addr: "/tmp/redis.sock"})
+	defer client.Close()
+	msg, _ := msgpack.Marshal(Measurement{value: 123.456, timestamp: 123456.7890})
+	client.Append("testmp", string(msg))
+	inq := make(chan []byte, 1000000)
+	mets := make(chan Metric, 1000000)
+	go startListening(inq, mets)
+
+	loopstart := time.Now()
+	var loopcount uint64
+	var wg sync.WaitGroup
+
+	for i := 0; i < WORKER_COUNT; i++ {
+		wg.Add(1)
+		go handleMetric(inq, mets, client, &loopcount, &loopstart, &wg)
+	}
+
+	for i := 0; i < WORKER_COUNT; i++ {
+		wg.Add(1)
+		go trimMetrics(MAX_METRICS, client, TRIM_INTERVAL)
+	}
+	wg.Wait()
+}
---
+++ test/test_client.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"math/rand"
+	"net"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func spawnClient(metricNames []string, port string, wg *sync.WaitGroup) {
+	wg.Add(1)
+	defer wg.Done()
+
+	ServerAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:"+port)
+	check(err)
+
+	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	check(err)
+
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	check(err)
+
+	defer Conn.Close()
+	for {
+		timestamp := strconv.FormatInt(time.Now().UnixNano(), 10)
+		val := strconv.FormatFloat(rand.Float64(), 'f', 16, 64)
+		name := metricNames[rand.Intn(len(metricNames))]
+
+		msg := []byte(name + " " + val + " " + timestamp + " \n")
+		Conn.Write(msg)
+		time.Sleep(time.Millisecond * 1)
+	}
+}
+
+func randMetricName(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	THOUSAND_METS_PER_SEC := 80
+	METRIC_NAME_LENGTH := 32
+	NUM_METRICS := 500000
+	PORT := "2001"
+
+	var metricsList []string
+	for i := 0; i < NUM_METRICS; i++ {
+		metricsList = append(metricsList, randMetricName(METRIC_NAME_LENGTH))
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < THOUSAND_METS_PER_SEC; i++ {
+		go spawnClient(metricsList, PORT, &wg)
+	}
+
+	wg.Wait()
+}
